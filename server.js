43cfda9dbfa7af9bdc4bb934d5cfd3f87cd155b8
Austin Limanek <austin.limanek@gmail.com>
2022-08-03 01:54:28 -0700

    created a more useful object from the flightapi response object to respond to the client

--- server.js
+++ server.js
@@ -50,9 +50,46 @@ app.get('/hotels', async (request, response, next) => {
 })
 
 app.get('/flights', async (request, response, next) => {
+  
   try{
-    let info = await axios.get('https://api.flightapi.io/onewaytrip/62e97e44392496cf5f252528/HEL/OUL/2022-08-20/1/0/0/Economy/USD');
-    console.log(info.data.fares);
+    let info = await axios.get('https://api.flightapi.io/onewaytrip/62e97e44392496cf5f252528/SEA/LON/2022-08-20/1/0/0/Economy/USD');
+    console.log(info.data.scores);
+    //obj: (object) All of the trip scores, higher is better
+    let obj = info.data.scores;
+    // optimalFlightString: (string) id for the best flight
+    let optimalFlightString = Object.entries(obj).sort((a,b) => {
+      if(a[1] < b[1]){
+        return 1;
+      }
+      else if(a[1] > b[1]){
+        return -1;
+      }
+      else{
+        return 0;
+      }
+    })[0][0];
+    console.log(optimalFlightString);
+    // topFlightArray: (array) All of the different prices for the same flight path - the websites have different prices
+    let topFlightArray = info.data.fares.filter(e => e.tripId === optimalFlightString);
+    console.log(topFlightArray);
+    //legId: (string) Used to connect price to flight info
+    let legId = info.data.trips.find(e => e.id === optimalFlightString).legIds[0];
+    console.log(legId);
+    //flightObj: (obj) Contains information about departure, arrival, airports, etc.
+    let flightObj = info.data.legs.find(e => e.id === legId);
+    console.log(flightObj);
+
+    //To be sent back to the front end
+    let reObject = {
+      total: topFlightArray[0].price.totalAmount,
+      bookingUrl: topFlightArray[0].handoffUrl,
+      departureTime: flightObj.departureTime,
+      arrivalTime: flightObj.arrivalTime,
+      stopOverCount: flightObj.stopoversCount
+    }
+    //Sending to the client
+    response.send(reObject);
+    console.log(reObject);
   }
   catch(e){
     console.log(e)

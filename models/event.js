1bfb079adcabb1faff30f525fcb9feba2712253f
Austin Limanek <austin.limanek@gmail.com>
2022-08-03 23:08:31 -0700

    deleted unneeded console.logs and added some notes for code readability. In addition, I added some additional ways of accessing objects from mongoDB. Finally, I reformatted the events scheme to accept journies

--- models/event.js
+++ models/event.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
 const eventSchema = new Schema({
-  location: String,
-  weather: String,
-  airfare: Number
+  userId: Number,
+  groupId: Number,
+  event: {},
+  flight: {},
+  hotels: {}
 });
 
 module.exports = model('Event', eventSchema);
--- server.js
+++ server.js
@@ -6,15 +6,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const Event = require('./models/event.js');
 const { default: axios } = require('axios');
-
 mongoose.connect(process.env.DATABASE_URL)
-
 const PORT = process.env.PORT || 3001
-
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
 class Hotels {
@@ -31,19 +26,12 @@ class Events {
     this.startTime = obj.start.slice(11, 19)
     this.endDate = obj.end.slice(0, 10)
     this.endTime = obj.end.slice(11, 19)
-    // this.description = obj.description || obj.entities[0].description
-    // this.address = obj.entities[0].formatted_address || obj.entities[1].formatted_address
   }
 }
 
 app.get('/hotels', async (request, response, next) => {
 
-  //let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query.body;
-  //let eventType = 'academic';
-  //let depLocation = 'seattle';
   let arrLocation = request.query.city;
-  //let fromDate = '2022-08-07';
-  //let toDate = '2022-08-14';
 
   let header = {
     "username": "erexie",
@@ -51,18 +39,15 @@ app.get('/hotels', async (request, response, next) => {
   }
 
   try {
-    let tokendata = await axios.post('https://api.makcorps.com/auth', header)
-    let authToken = {
-      Authorization: `JWT ${tokendata.data.access_token}`
-    }
+    let tokenData = await axios.post('https://api.makcorps.com/auth', header)
 
     let config = {
-      headers: { Authorization: `JWT ${tokendata.data.access_token}` },
+      headers: { Authorization: `JWT ${tokenData.data.access_token}` },
       url: `https://api.makcorps.com/free/${arrLocation}`,
       method: 'get'
     }
     await axios(config)
-let res = await axios(config)
+    let res = await axios(config)
     const responseTrimmed = (res.data.Comparison.slice(0, 5));
     let hotelData = responseTrimmed.map(hotel => new Hotels(hotel))
     
@@ -71,15 +56,9 @@ let res = await axios(config)
   } catch (e) {
     console.log(e)
   }
-
-  // Event.find()
-  //   .then(eventEnsemble => {
-  //     response.send(eventEnsemble);
-  //   })
 })
 
 app.get('/flights', async (request, response, next) => {
-console.log(request.query)
 let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query;
 //let eventType = 'academic';
 // let depLocation = 'seattle';
@@ -94,13 +73,13 @@ let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query;
     // console.log(depAirport);
 
     let depIata = depAirport.data.data[0].iata;
-    // console.log(depIata);
     let arrIata = arrAirport.data.data[0].iata;
 
     let info = await axios.get(`https://api.flightapi.io/onewaytrip/${process.env.FLIGHT_API}/${depIata}/${arrIata}/${fromDate}/1/0/0/Economy/USD`);
-    // console.log(info.data.scores);
+
     //obj: (object) All of the trip scores, higher is better
     let obj = info.data.scores;
+
     // optimalFlightString: (string) id for the best flight
     let optimalFlightString = Object.entries(obj).sort((a,b) => {
       if(a[1] < b[1]){
@@ -113,16 +92,15 @@ let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query;
         return 0;
       }
     })[0][0];
-    // console.log(optimalFlightString);
+
     // topFlightArray: (array) All of the different prices for the same flight path - the websites have different prices
     let topFlightArray = info.data.fares.filter(e => e.tripId === optimalFlightString);
-    // console.log(topFlightArray);
+
     //legId: (string) Used to connect price to flight info
     let legId = info.data.trips.find(e => e.id === optimalFlightString).legIds[0];
-    // console.log(legId);
+    
     //flightObj: (obj) Contains information about departure, arrival, airports, etc.
     let flightObj = info.data.legs.find(e => e.id === legId);
-    // console.log(flightObj);
 
     //To be sent back to the front end
     let reObject = {
@@ -132,6 +110,7 @@ let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query;
       arrivalTime: flightObj.arrivalTime,
       stopOverCount: flightObj.stopoversCount
     }
+    
     //Sending to the client
     response.send(reObject);
   }
@@ -142,23 +121,20 @@ let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query;
 
 app.get('/events', async (request, response, next) => {
   let {eventType, depLocation, arrLocation, fromDate, toDate } = request.query;
-  // let eventType = 'sports';
-  //let depLocation = 'seattle';
-  // let arrLocation = 'seattle';
-  // let fromDate = '2022-08-07';
-  // let toDate = '2022-08-14';
-  //let query = ''
+
   try {
+    // Finding the city id of the arrLocation
     let config = {
       headers: { Authorization: `Bearer ${process.env.BEARER_API}` },
       url: `https://api.predicthq.com/v1/places/?q=${arrLocation}`,
       method: 'get'
     }
     let res = await axios(config)
-    // console.log(res)
-    let id = res.data.results[0].id;
-    // console.log(id);
 
+    //Pulling out the id from the result
+    let id = res.data.results[0].id;
+
+    //Using id and other query data to find events
     let config2 = {
       headers: { Authorization: `Bearer ${process.env.BEARER_API}` },
       url: `https://api.predicthq.com/v1/events/?place.scope=${id}&active.gte=${fromDate}&active.lte=${toDate}&category=${eventType}&sort=rank`,
@@ -166,10 +142,13 @@ app.get('/events', async (request, response, next) => {
     }
     let res2 = await axios(config2);
 
-    // console.log(res2.data.results[0])
-
+    //Parsing response data
     let responseTrimmed = res2.data.results.slice(0,5);
+
+    //Saving each event as an event object
     let eventData = responseTrimmed.map(event => new Events(event));
+
+    //Sending back all events to client  dx
     response.send(eventData);
   } 
   
@@ -179,21 +158,58 @@ app.get('/events', async (request, response, next) => {
 
 })
 
+app.get('/allEvents', async (request, response, next) => {
+  request + 1;
+  try {
+    let events = await Event.find({});
+    response.send(events);
+  }
+  catch (e) {
+    next(e);
+  }
 
+})
+
+
+app.get('/groupEvents/:group', async (request, response, next) => {
+  let id = request.params.group;
+  try {
+    let groupEvents = await Event.find({ groupId: id});
+    response.send(groupEvents);
+  }
+  catch (e) {
+    next(e);
+  }
+
+})
+
+app.get('/userEvents/:user', async (request, response, next) => {
+  let id = request.params.user;
+  try {
+    let groupEvents = await Event.find({  userId: id });
+    response.send(groupEvents);
+  }
+  catch (e) {
+    next(e);
+  }
+
+})
 
 
 app.post('/events', (request, response, next) => {
   console.log(request.body);
-  let { location, weather, airfare } = request.body;
-  if (!location) {
+  let { userId, groupId, event, flight, hotels } = request.body;
+  if (!userId) {
     next('bad request');
   }
-  let event = new Event({
-    location,
-    weather,
-    airfare: parseInt(airfare)
+  let journey = new Event({
+    userId,
+    groupId,
+    event,
+    flight,
+    hotels
   })
-  event.save()
+  journey.save()
     .then(results => {
       console.log(results);
       response.send(results);
